219fbe0287d3b5b8b1f4d36f862e613057d4f7fe
Selecia aaa <garrisonroniece@gmail.com>
2023-08-03 09:44:05 -0400

    repository cloned

---
+++ script.js
@@ -0,0 +1,19 @@
+// Assignment code here
+var password=document.getElementById("password");
+var characters="0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var passwordLength = 0 - 128;
+var password = ""
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
